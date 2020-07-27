0c80d5b04f62a3e86ea50b5f096a38ae0f2ab062
Albin Eriksson <40474222+kezoponk@users.noreply.github.com>
2020-07-27 17:20:19 +0200

    Update bytesorter.java

--- bytesorter.java
+++ bytesorter.java
@@ -1,87 +1,133 @@
 import java.util.Arrays;
-import java.util.Scanner;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyAdapter;
+import javax.swing.JScrollPane;
+import java.awt.Component;
+import javax.swing.ScrollPaneConstants;
 
 public class bytesorter {
 
-    static int index = 0;
-    static int lang = 0;
-    static byte Bytes[] = new byte[200];
-    static byte unsortedBytes[] = new byte[200];
-    static String unsortedStrings[] = new String[200];
-    static String sortedBytes[] = new String[100];
+  static JFrame frame;
+  static JScrollPane inputScroll, outputScroll;
+  static JTextArea input, output;
+  static int index, lang = 0;
 
-    static int realLength(String[] arr) {
-        int count = 0;
-        for (String out : arr) {
-            try {
-                if (out.isBlank()) return count;
-            } catch (NullPointerException e) {
-                return count;
-            }
-            count++;
-        }
+  static byte Bytes[] = new byte[200];
+  static byte unsortedBytes[] = new byte[200];
+  static String unsortedStrings[] = new String[200];
+  static String sortedBytes[] = new String[100];
+
+  static int realLength(String[] arr) {
+    int count = 0;
+    for (String out : arr) {
+      try {
+        if (out.isBlank()) return count;
+      } catch (NullPointerException e) {
         return count;
+      }
+      count++;
     }
+    return count;
+  }
 
-    static String javaAndPythonSplitter(String line) {
-        String type = "";
-        String trimmed = "";
+  static String javaAndPythonSplitter(String line) {
+    String type = "";
+    String trimmed = "";
 
-        if (line.contains("& 0b")) type = "b";      // Byte
-        else if (line.contains("& 0x")) type = "x"; // Hex
-        String splittedLine[] = line.split("& 0"+type);
+    if (line.contains("& 0b")) type = "b";      // Byte
+    else if (line.contains("& 0x")) type = "x"; // Hex
+    String splittedLine[] = line.split("& 0"+type);
 
-        for (String character : splittedLine[1].split("")) {
-            if(character.equals(")") || character.equals(" ")) break;
-            trimmed += character;
-        }
-        if (trimmed.split("").length < 2) {
-            System.out.println("Invalid byte selection");
-            System.exit(1);
-        }
-        return trimmed;
+    for (String character : splittedLine[1].split("")) {
+      if(character.equals(")") || character.equals(" ")) break;
+      trimmed += character;
     }
+    return trimmed;
+  }
 
-    static void getBytes(String line) {
-        Bytes[index] = (byte) (-1*Byte.parseByte(javaAndPythonSplitter(line), 2));
-        unsortedStrings[index] = line;
-        unsortedBytes[index] = Bytes[index];
-        index++;
-    }
+  static void getBytes(String line) {
+    Bytes[index] = (byte) (-1*Byte.parseByte(javaAndPythonSplitter(line), 2));
+    unsortedStrings[index] = line;
+    unsortedBytes[index] = Bytes[index];
+    index++;
+  }
 
-    static void sortBytes() {
-        int index = 0, lineIndex = 0;
-        Arrays.sort(Bytes);
+  static void sortBytes() {
+    int index = 0, lineIndex = 0;
+    Arrays.sort(Bytes);
 
-        while(index<realLength(unsortedStrings)) {
-            System.out.println(index);
-            lineIndex = 0;
+    while(index<realLength(unsortedStrings)) {
+      lineIndex = 0;
 
-            for (int unsortedByte: unsortedBytes) {
-                if((byte) Bytes[index] == unsortedByte) {
-                    sortedBytes[index] = unsortedStrings[lineIndex];
-                    break;
-                }
-                lineIndex++;
-            }
-            index++;
+      for (int unsortedByte: unsortedBytes) {
+        if((byte) Bytes[index] == unsortedByte) {
+          sortedBytes[index] = unsortedStrings[lineIndex];
+          break;
         }
-        for (String line: sortedBytes) {
-            if(line == null) break;
-            System.out.println(line);
-        }
+        lineIndex++;
+      }
+      index++;
     }
+    for (String line: sortedBytes) {
+      if(line == null) break;
+      output.append(line+"\n");
+    }
+  }
 
-    public static void main(String[] args) {
-        System.out.println("Tested with:\nJava\nPython\n");
-        System.out.println("Paste if selection with bytes to sort, press space -> enter when finished");
-        Scanner scanner = new Scanner(System.in);
-        scanner.useDelimiter("\n");
+  static JScrollPane textarea(int bounds[], JTextArea textarea) {
+    JScrollPane scrollpane = new JScrollPane((Component) null);
+    scrollpane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+    scrollpane.setBounds(bounds[0], bounds[1], bounds[2], bounds[3]);
+
+    textarea.setForeground(new Color(0, 0, 0));
+    textarea.setBackground(Color.WHITE);
+    textarea.setLineWrap(true);
+    textarea.setFont(new Font("Calibri", 200, 10));
+
+    scrollpane.setViewportView(textarea);
+    return scrollpane;
+  }
+
+  public static void main(String[] args) {
+    frame = new JFrame();
+    frame.getContentPane().setForeground(Color.WHITE);
+    frame.setBounds(100, 100, 550, 455);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.getContentPane().setLayout(null);
+
+    input = new JTextArea();
+    output = new JTextArea();
+
+    int inputBounds[] = {10, 10, 530, 200};
+    inputScroll = textarea(inputBounds, input);
+    frame.add(inputScroll);
+
+    input.addKeyListener(new KeyAdapter() {
+      public void keyReleased(KeyEvent x) {
+        output.setText("");
+        sortedBytes = new String[100];
+        Bytes = new byte[200];
+        index = 0;
+
         try {
-            scanner.forEachRemaining(line -> getBytes(line));
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("\nResult: \n");
+          String inputs = input.getText();
+          for (String line: inputs.split("\n")) {
+            getBytes(line);
+          }
+          sortBytes();
+        } catch(Exception e) {
+          output.setText("Invalid byte selection");
         }
-        sortBytes();
-    }
+      }
+    });
+    int outputBounds[] = {10, 220, 530, 200};
+    outputScroll = textarea(outputBounds, output);
+    frame.add(outputScroll);
+
+    frame.setVisible(true);
+  }
 }
